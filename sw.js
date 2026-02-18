bb1eedcbb9fd011a7d34f60300c8b3235c46eb61
Worksduvi <125094799+Worksduvi@users.noreply.github.com>
2026-02-19 00:25:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,21 @@
-const CACHE_NAME = "vicomedia-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon.png"
+const CACHE_NAME = 'vico-media-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.png',
+    'https://unpkg.com/lucide@latest',
+    'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Roboto:wght@300;400&display=swap'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
